d98da47c77e083d2e193363db2eaae6383beb3b4
raulgodii <raulgodii13@gmail.com>
2024-02-05 14:30:20 +0100

    pedircita route

--- app/Http/Controllers/CitasController.php
+++ app/Http/Controllers/CitasController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class CitasController extends Controller
 {
-    
+    public function pedirCita()
+    {
+        // Lógica para la funcionalidad de pedir cita
+        // Puedes mostrar un formulario para pedir la cita o realizar otras acciones relacionadas.
+
+        return view('pedir-cita'); // Ajusta el nombre de la vista según sea necesario.
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::delete('/pedircita', [CitasController::class], 'pedirCita')->name('profile.destroy');
+    Route::get('/pedircita', [CitasController::class], 'pedirCita')->name('profile.destroy');
 
 });
 
