d6dd3efda4dfcdc53c93d6356b73f5b5eb6e2e85
raulgodii <raulgodii13@gmail.com>
2024-02-05 13:59:22 +0100

    DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(PeluquerosSeeder::class);
+        $this->call(ServiciosSeeder::class);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
