c513f62e08a7d0c800e023b1a5ab9153af7bcea7
raulgodii <raulgodii13@gmail.com>
2024-02-05 13:41:34 +0100

    citas

--- database/migrations/2024_02_05_121229_create_citas_table.php
+++ database/migrations/2024_02_05_121229_create_citas_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
     {
         Schema::create('citas', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('nombre_usuario');
-            $table->unsignedBigInteger('nombre_peluquero');
-            $table->unsignedBigInteger('nombre_servicio');
+            $table->unsignedBigInteger('user_id'); // Cambié el nombre a user_id
+            $table->unsignedBigInteger('peluquero_id');
+            $table->unsignedBigInteger('servicio_id');
             $table->dateTime('fecha');
             $table->timestamps();
-
-            $table->foreign('nombre_usuario')->references('name')->on('users');
-            $table->foreign('nombre_peluquero')->references('nombre')->on('peluqueros');
-            $table->foreign('nombre_servicio')->references('nombre')->on('servicios');
+        
+            $table->foreign('user_id')->references('id')->on('users'); // Referencia a la columna 'id' en la tabla 'users'
+            $table->foreign('peluquero_id')->references('id')->on('peluqueros'); // Referencia a la columna 'id' en la tabla 'peluqueros'
+            $table->foreign('servicio_id')->references('id')->on('servicios'); // Referencia a la columna 'id' en la tabla 'servicios'
         });
     }
 
